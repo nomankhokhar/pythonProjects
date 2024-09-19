9b6d40f713be0207d78c829a71fcd6258b452204
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-09-19 23:26:58 +0500

    QR Code Generator Improve Version

--- QR Code Generator/main.py
+++ QR Code Generator/main.py
@@ -1,19 +1,53 @@
-import qrcode as qr
+import qrcode
+
+from PIL import Image
 
 def generate_qr_code(data, file_name):
     try:
-        img = qr.make(data)
-        img.save(file_name+".png")
-    except Exception as e:  
-        print("Error in generating QR code. Please try again.")
-    
-    
-    
-if __name__ == '__main__':
-    try:    
-        data = input("Enter the data to be stored in the QR code: ")
-        file_name = input("Enter the name of the file to save the QR code: ")
-        generate_qr_code(data, file_name)
-        print("QR code generated successfully!")
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=5,
+        )
+        
+        qr.add_data(data)
+        qr.make(fit=True)
+        
+        img = qr.make_image(fill_color="black", back_color="white")
+        
+        try:
+            logo = Image.open("logo.png")
+            basewidth = 70
+            wpercent = (basewidth / float(logo.size[0]))
+            hsize = int((float(logo.size[1]) * float(wpercent)))
+            logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
+            
+            img = img.convert("RGBA")
+            logo = logo.convert("RGBA")
+            
+            pos = ((img.size[0] - logo.size[0]) // 2, (img.size[1] - logo.size[1]) // 2)
+            
+            img.paste(logo, pos, logo)
+            
+        except FileNotFoundError:
+            print("Logo file not found")
+        except  Exception as e:
+            print("Error with logo processing: ", e)
+            
+        img.save(file_name + ".png")
+        
     except Exception as e:
-        print("Error in generating QR code. Please try again.")
+        print("Error with QR code generation: ", e)
+        
+        
+
+if __name__ == "__main__":
+    try:
+        data = input("Enter data to encode: ")
+        file_name = input("Enter file name: ")
+        
+        generate_qr_code(data, file_name)
+    except Exception as e:
+        print("Error: ", e)
+            
